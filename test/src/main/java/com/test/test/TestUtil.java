231b8574816c26e40e6e1919f5bdbb93b381a6cd
zhengcaifu <344984602@qq.com>
2020-12-01 16:11:32 +0800

    Initial commit

---
+++ test/src/main/java/com/test/test/TestUtil.java
@@ -0,0 +1,11 @@
+package com.test.test;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class TestUtil {
+
+    public static void showTest(Context context) {
+        Toast.makeText(context, "test", Toast.LENGTH_LONG).show();
+    }
+}
